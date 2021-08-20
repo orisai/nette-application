a86003faa80c6d620275b872e58d9966c2d775d3
Marek Bartoš <bartos.developer152@gmail.com>
2021-08-20 11:25:29 +0200

    ComponentInspectorExtension: active->enabled, coding style

--- ComponentInspectorExtension.php
+++ ComponentInspectorExtension.php
@@ -1,63 +1,70 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 namespace Extension\ComponentInspector;
 
 use Nette\Application\UI\TemplateFactory;
 use Nette\DI\CompilerExtension;
-use Nette\DI\Definitions\Reference;
 use Nette\DI\Definitions\ServiceDefinition;
 use Nette\Schema\Expect;
 use Nette\Schema\Schema;
 use Tracy\Bar;
 use Tracy\Debugger;
 
-class ComponentInspectorExtension extends CompilerExtension
+final class ComponentInspectorExtension extends CompilerExtension
 {
-    public function getConfigSchema(): Schema
-    {
-        Debugger::getBar()->addPanel(new InspectPanel());
 
-        return Expect::structure([
-            'active' => Expect::bool(false),
-        ]);
-    }
+	public function getConfigSchema(): Schema
+	{
+		Debugger::getBar()->addPanel(new InspectPanel());
 
-    public function loadConfiguration()
-    {
-        if ($this->config->active) {
-            $builder = $this->getContainerBuilder();
+		return Expect::structure([
+			'enabled' => Expect::bool(false),
+		]);
+	}
 
-            $factoryName = $builder->getByType(TemplateFactory::class);
-            $factory = $builder->getDefinition($factoryName);
-            $factory->setAutowired(false);
+	public function loadConfiguration(): void
+	{
+		parent::loadConfiguration();
 
-            $definition = new ServiceDefinition();
-            $definition
-                ->setFactory(InspectTemplateFactory::class)
-                ->addSetup('setOriginalFactory', [$factoryName]);
+		if (!$this->config->enabled) {
+			return;
+		}
 
-            $builder->addDefinition($this->prefix('templateFactory'), $definition);
-        }
-    }
+		$builder = $this->getContainerBuilder();
 
-    public function beforeCompile()
-    {
-        if ($this->config->active) {
-            $builder = $this->getContainerBuilder();
+		$factoryName = $builder->getByType(TemplateFactory::class);
+		$factory = $builder->getDefinition($factoryName);
+		$factory->setAutowired(false);
 
-            $tracyPanel = $builder->addDefinition($this->prefix('tracy.panel'))
-                ->setFactory(InspectPanel::class)
-                ->setAutowired(false);
+		$definition = new ServiceDefinition();
+		$definition
+			->setFactory(InspectTemplateFactory::class)
+			->addSetup('setOriginalFactory', [$factoryName]);
 
-            $this->getInitialization()->addBody(
-                '$this->getService(?)->addPanel($this->getService(?));',
-                [
-                    $builder->getByType(Bar::class),
-                    $tracyPanel->getName(),
-                ],
-            );
-        }
-    }
+		$builder->addDefinition($this->prefix('templateFactory'), $definition);
+	}
+
+	public function beforeCompile(): void
+	{
+		parent::beforeCompile();
+
+		if (!$this->config->enabled) {
+			return;
+		}
+
+		$builder = $this->getContainerBuilder();
+
+		$tracyPanel = $builder->addDefinition($this->prefix('tracy.panel'))
+			->setFactory(InspectPanel::class)
+			->setAutowired(false);
+
+		$this->getInitialization()->addBody(
+			'$this->getService(?)->addPanel($this->getService(?));',
+			[
+				$builder->getByType(Bar::class),
+				$tracyPanel->getName(),
+			],
+		);
+	}
+
 }
