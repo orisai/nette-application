a8cb03a665d35c078c103448997df83420d653fb
Marek Bartoš <bartos.developer152@gmail.com>
2021-09-26 16:10:51 +0200

    StrictPresenterFactoryCallback: support inheritance of presenters when both are registered as services

--- src/Mapping/StrictPresenterFactoryCallback.php
+++ src/Mapping/StrictPresenterFactoryCallback.php
@@ -5,7 +5,8 @@ namespace OriNette\Application\Mapping;
 use Nette\Application\InvalidPresenterException;
 use Nette\Application\IPresenter;
 use Nette\DI\Container;
-use function array_shift;
+use function array_keys;
+use function array_map;
 use function assert;
 use function count;
 use function implode;
@@ -21,6 +22,38 @@ final class StrictPresenterFactoryCallback
 		$this->container = $container;
 	}
 
+	/**
+	 * @param array<string> $services
+	 */
+	private function getServiceName(array $services, string $class): string
+	{
+		if (count($services) === 1) {
+			return $services[0];
+		}
+
+		$exact = array_keys(
+			array_map(
+				fn (string $name): string => $this->container->getServiceType($name),
+				$services,
+			),
+			$class,
+			true,
+		);
+
+		if (count($exact) === 1) {
+			return $services[$exact[0]];
+		}
+
+		throw new InvalidPresenterException(sprintf(
+			'Multiple services of type "%s" found: %s.',
+			$class,
+			implode(', ', $services),
+		));
+	}
+
+	/**
+	 * @param class-string<IPresenter> $class
+	 */
 	public function __invoke(string $class): IPresenter
 	{
 		$services = $this->container->findByType($class);
@@ -32,15 +65,9 @@ final class StrictPresenterFactoryCallback
 			));
 		}
 
-		if (count($services) !== 1) {
-			throw new InvalidPresenterException(sprintf(
-				'Multiple services of type "%s" found: %s.',
-				$class,
-				implode(', ', $services),
-			));
-		}
-
-		$service = $this->container->createService(array_shift($services));
+		$service = $this->container->createService(
+			$this->getServiceName($services, $class),
+		);
 		assert($service instanceof IPresenter);
 
 		return $service;
