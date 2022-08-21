882de00c776a87a7b0640b527063d9aba92a15ca
Pepa Linha <josef.linha@gmail.com>
2022-08-21 11:19:45 +0200

    fixup! fixup! fixup! fixup! Prepare Tracy panels in Svelte

--- ui/src/lib/inspector/ComponentDetail.svelte
+++ ui/src/lib/inspector/ComponentDetail.svelte
@@ -2,41 +2,51 @@
 	import RenderTime from "../RenderTime.svelte";
 
 	export let component
+
+	let isError = false
+
+	// @todo
+	if (!component || !component.tree || !component.renderTime) {
+		isError = true
+	}
 </script>
 
-<h2>{component.name}</h2>
+{#if isError}
+	Chyba @todo
+{:else}
+	<h2>{component.name}</h2>
+	{#if component.tree.length > 0}
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Class</th>
+					<th>Template</th>
+				</tr>
+			</thead>
+			{#each component.tree as info}
+				<tr>
+					<td>{info.name}</td>
+					<td>
+						<a href="{info.file}">
+							{info.className}
+						</a>
+					</td>
+					<td>
+						<a href="{info.templateFile}">
+							{info.templateFileName}
+						</a>
+					</td>
+				</tr>
+			{/each}
+		</table>
+	{/if}
 
-{#if component.tree.length > 0}
-	<table>
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Class</th>
-				<th>Template</th>
-			</tr>
-		</thead>
-		{#each component.tree as info}
-			<tr>
-				<td>{info.name}</td>
-				<td>
-					<a href="{info.file}">
-						{info.className}
-					</a>
-				</td>
-				<td>
-					<a href="{info.templateFile}">
-						{info.templateFileName}
-					</a>
-				</td>
-			</tr>
-		{/each}
-	</table>
-{/if}
-
-{#if component.renderTime}
-	<div>
-		<RenderTime timeInSeconds={component.renderTime}/>
-	</div>
+	{#if component.renderTime}
+		<div>
+			<RenderTime timeInSeconds={component.renderTime}/>
+		</div>
+	{/if}
 {/if}
 
 <style lang="sass">
